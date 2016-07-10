8bdbf3a0f1f1c50c5e4a8966a528898da465a3f3
Sprax Lines <sprax@comcast.net>
2016-07-10 15:35:26 -0400

    flatten

---
+++ flatten.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+# S.R. Lines   2004  3/12 - 3/27
+
+sub flatten(@list) {
+    
+}
+
+
+# printf "%2d  $nn[$j].$nn[$k]\n", ++$z;
+    }
+}
+$z = 0;
+print "shift\n";
+while (@nn) {
+    $a = shift @nn;
+    for $b (@nn) {
+        printf "%2d  $a.$b\n", ++$z;
+    }
+}
+
+exit;
+
