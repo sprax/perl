939f8bd725a728ecc2debe0f916edeb466e46f20
Sprax Lines <sprax@comcast.net>
2016-07-11 10:25:47 -0400

    perlmonks

--- flatten.pl
+++ flatten.pl
@@ -1,22 +1,9 @@
 #!/usr/bin/perl -w
 # S.R. Lines   2004  3/12 - 3/27
 
-sub flatten(@list) {
-    
-}
+sub flatten { map { ref $_ eq 'ARRAY' ? flatten(@$_) : $_ } @_ }
 
-
-# printf "%2d  $nn[$j].$nn[$k]\n", ++$z;
-    }
-}
-$z = 0;
-print "shift\n";
-while (@nn) {
-    $a = shift @nn;
-    for $b (@nn) {
-        printf "%2d  $a.$b\n", ++$z;
-    }
-}
+print join ', ' => flatten 1, 2, [3,4], [5, [6,7, [8,"foosball",9] ],10], "\n";
 
 exit;
 
